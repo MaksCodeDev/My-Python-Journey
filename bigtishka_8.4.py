b8d4d3e25e7764494929e0b1a90452faba8f6670
Maximus <m20007383@gmail.com>
2026-03-05 17:22:04 +0500

    Add files via upload

---
+++ bigtishka_8.4.py
@@ -0,0 +1,5 @@
+def make_shirit(size, text):
+    """Выводится информация о футболке"""
+    print(f"Размен футболки: {size},\n текст на футболке: {text}")
+
+make_shirit(size='L', text='Я люблю python')
