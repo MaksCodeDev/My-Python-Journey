9e87ba739425b1fd885e2107eb94a0324f7a7bb4
Maximus <m20007383@gmail.com>
2026-03-10 21:04:27 +0500

    Add files via upload

---
+++ message_8.9.py
@@ -0,0 +1,6 @@
+def show_messages(messages):
+    for message in messages:
+        print(message)
+my_list = ['привет', 'я учу питон', 'у меня скоро экзамены', 'как дела']
+
+show_messages(my_list)
